c3c4da2559cfdfe1c2118d61e64bdd47a635cb1c
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2023-03-08 09:54:33 -0400

    initial commit (#3)

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'UI Definitions Docs',
+  description: 'A user interface definition documentation based on components, compositions, and pattern layouts.',
+})
