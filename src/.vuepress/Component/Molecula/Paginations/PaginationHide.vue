fe5b769e9a8bc5a114deacba1be58d6f528b7091
elsiosanchez <elsiosanches@gmail.com>
2023-03-22 14:53:36 -0400

    minimal changes

--- src/.vuepress/Component/Molecula/Paginations/PaginationHide.vue
+++ src/.vuepress/Component/Molecula/Paginations/PaginationHide.vue
@@ -1,19 +1,18 @@
 <template>
   <el-card class="box-card">
     <div>
-			<el-switch v-model="value" />
-			<hr class="my-4" />
-			<el-pagination
-				v-show="value"
-				:total="50"
-				layout="prev, pager, next"
-			/>
-		</div>
+      <el-switch v-model="value" />
+      <hr class="my-4" />
+      <el-pagination
+        v-show="value"
+        :total="50"
+        layout="prev, pager, next"
+      />
+    </div>
   </el-card>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 const value = ref(true)
-
 </script>
